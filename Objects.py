e851fcc49e7eaf029eaa4cfdbbeb5a598cb26d14
Fagin Hales <fhales@lcn.ucl.ac.uk>
2018-02-10 16:24:05 +0000

    Created classes

--- Objects.py
+++ Objects.py
@@ -5,3 +5,30 @@ Created on Sat Feb 10 15:56:45 2018
 @author: fhales
 """
 
+class player:
+    def __init__(self, health, money, deck, items):
+        self.health = health
+        self.money = money
+        self.deck = deck
+        self.items = items
+
+class deck:
+    def __init__(self, cards):
+        self.cards = cards
+
+class card:
+    def __init__(self, cost, effect, rarity):
+        self.cost = cost
+        self.effect = effect
+        self.rarity = rarity
+
+class monster:
+    def __init__(self, health, attacks, challange):
+        self.health = health
+        self.attacks = attacks
+        self.challange = challange
+
+class attack:
+    def __init__(self, effect, power):
+        self.effect = effect
+        self.power = power
